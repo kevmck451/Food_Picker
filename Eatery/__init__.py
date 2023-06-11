aafffd753ff5e024840486f72ac0e4512b8683bc
KevMcK <kmcrunk@gmail.com>
2023-06-11 03:57:48 -0500

    Version 1.0

--- Eatery/__init__.py
+++ Eatery/__init__.py
@@ -6,3 +6,13 @@ Kevin McKenzie June 2023
 '''
 
 
+# __init__.py
+
+# Import specific classes or variables to make them available when importing the package
+from .eatery import Eatery
+from .places import *
+from .suggestion import Suggestion
+
+# Optionally, define any initialization code or perform setup tasks here
+# For example, you can print a message to indicate that the package is being initialized
+print("Eatery package initialized!")
